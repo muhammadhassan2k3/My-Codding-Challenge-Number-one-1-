3df85ef7656a1783f4d1270534ad5a6570a030c0
muhammad hassan <hassankhankhail90@gmail.com>
2022-09-30 20:16:37 +0500

    my javascript coding challenge number one(1)

---
+++ ChallengF2.js
@@ -0,0 +1,27 @@
+// const MarksWeight = 78;
+// const MarksHeight = 1.69;
+
+// const JohnWeight = 92;
+// const JohnHeight = 1.95;
+
+// const BMIMarksWeight = MarksWeight / (MarksHeight * MarksHeight);
+// const BMIjohnWeight = JohnWeight / (JohnHeight * JohnHeight);
+
+// console.log("BMI of mark's is ", BMIMarksWeight);
+// console.log("BMI of joh'n's is ", BMIjohnWeight);
+
+// console.log(BMIMarksWeight > BMIjohnWeight )
+
+const MarksWeight = 95;
+const MarksHeight = 1.88;
+
+const JohnWeight = 85;
+const JohnHeight = 1.76;
+
+const BMIMarksWeight = MarksWeight / (MarksHeight * MarksHeight);
+const BMIjohnWeight = JohnWeight / (JohnHeight * JohnHeight);
+
+console.log("BMI of mark's is ", BMIMarksWeight);
+console.log("BMI of joh'n's is ", BMIjohnWeight);
+
+console.log(BMIMarksWeight > BMIjohnWeight )
